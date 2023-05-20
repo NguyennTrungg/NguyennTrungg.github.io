7e5173a242b2ed76434e1a120cdcd36d4e6e2ede
Allex <20H1120095@ut.edu.vn>
2023-05-20 17:54:04 +0700

    update Cart

--- app/src/main/java/com/example/petshop/Cart.java
+++ app/src/main/java/com/example/petshop/Cart.java
@@ -1,16 +1,28 @@
 package com.example.petshop;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.example.petshop.adapter.MyCartAdapter;
@@ -43,6 +55,8 @@ public class Cart extends AppCompatActivity implements ICartLoadListener {
     ImageView btnBack;
     @BindView(R.id.tx_Total)
     TextView txTotal;
+    @BindView(R.id.btn_purchase)
+    Button btnPurchase;
 
     ICartLoadListener cartLoadListener;
 
@@ -91,9 +105,16 @@ public class Cart extends AppCompatActivity implements ICartLoadListener {
                                 cartModels.add(cartModel);
                             }
                             cartLoadListener.onCartLoadSuccess(cartModels);
+                            btnPurchase.setOnClickListener(v -> OpenDetailInfo(Gravity.CENTER));
                         }
                         else {
                             cartLoadListener.onCartLoadFailed("Cart Empty");
+                            btnPurchase.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Snackbar.make(mainLayout, "Your Cart is empty!", Snackbar.LENGTH_LONG).show();
+                                }
+                            });
                         }
                     }
 
@@ -113,6 +134,73 @@ public class Cart extends AppCompatActivity implements ICartLoadListener {
         recyclerCart.addItemDecoration(new DividerItemDecoration(this,layoutManager.getOrientation()));
 
         btnBack.setOnClickListener(v -> finish());
+    }
+
+    private void OpenDetailInfo(int gravity){
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.layout_purchase);
+        Button btnYes = dialog.findViewById(R.id.btn_Yes);
+        Button btnNo = dialog.findViewById(R.id.btn_No);
+        EditText edName = dialog.findViewById(R.id.ed_Name);
+        EditText edPhone = dialog.findViewById(R.id.ed_PhoneNum);
+        EditText edAddress = dialog.findViewById(R.id.ed_Address);
+
+        Window window = dialog.getWindow();
+        if (window == null){
+            return;
+        }
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams windownAttributes = window.getAttributes();
+        windownAttributes.gravity = gravity;
+        window.setAttributes(windownAttributes);
+
+        btnNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        btnYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edName.getText().toString().compareTo("")!= 0
+                        & edPhone.getText().toString().compareTo("")!= 0
+                        & edAddress.getText().toString().compareTo("")!= 0){
+                    if (edPhone.getText().toString().length() == 11 || edPhone.getText().toString().length() == 10 ) {
+                        dialog.cancel();
+                        FirebaseDatabase.getInstance()
+                                .getReference("Cart")
+                                .child("UNIQUE_USER_ID")
+                                .removeValue()
+                                .addOnSuccessListener(aVoid -> EventBus.getDefault().postSticky(new MyUpdateCartEvent()));
+
+                        AlertDialog.Builder builder1 = new AlertDialog.Builder(Cart.this);
+                        builder1.setTitle("Thanks for your Purchase!")
+                                .setMessage("Your Purchase will be sent to your address as soon as possible!")
+                                .setCancelable(true)
+                                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                        finish();
+                                        startActivity(getIntent());
+                                    }
+                                })
+                                .show();
+                    } else {
+                        Toast.makeText(Cart.this,"Phone number invalid!", Toast.LENGTH_SHORT).show();
+                    }
+
+                } else {
+                    Toast.makeText(Cart.this,"Info can't be empty!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        dialog.show();
     }
 
     @Override
