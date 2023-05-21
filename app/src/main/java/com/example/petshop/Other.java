583c1b6e1f1fc9cc339a93064a082abcc4585aca
NguyennTrungg <89459592+NguyennTrungg@users.noreply.github.com>
2023-05-21 11:52:18 +0700

    update changepassword

--- app/src/main/java/com/example/petshop/Other.java
+++ app/src/main/java/com/example/petshop/Other.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,18 +14,29 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class Other extends AppCompatActivity implements View.OnClickListener{
-    private TextView appointmentSchedule;
+    RelativeLayout orderLayout;
+    private TextView appointmentSchedule, changePassword, btnLogout;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.orders);
 
+        orderLayout = (RelativeLayout) findViewById(R.id.order_layout);
+        changePassword = (TextView) findViewById(R.id.ed_ChangePass);
         appointmentSchedule = (TextView) findViewById(R.id.et_LichHen);
+        btnLogout = (TextView) findViewById(R.id.ed_LogOut);
+
+        changePassword.setOnClickListener(this);
         appointmentSchedule.setOnClickListener(this);
+        btnLogout.setOnClickListener(this);
 
         BottomNavigationView botNav = (BottomNavigationView) findViewById(R.id.nav);
         botNav.setSelectedItemId(R.id.Other);
@@ -47,32 +59,31 @@ public class Other extends AppCompatActivity implements View.OnClickListener{
                 return true;
             }
         });
-
-        TextView btnLogout = (TextView) findViewById(R.id.ed_LogOut);
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), SignUp_In.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        TextView txtProfile = (TextView) findViewById(R.id.ed_Profile);
-        txtProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), Profile.class));
-            }
-        });
     }
 
     @Override
     public void onClick(View v) {
-        if (v == appointmentSchedule){
-            Intent intent = new Intent(this, AppointmentSchedule.class);
+        if (v == appointmentSchedule)
+            startActivity(new Intent(this, AppointmentSchedule.class));
+        if (v == changePassword){
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            String emailAddress = user.getEmail();
+            auth.sendPasswordResetEmail(emailAddress)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Snackbar.make(orderLayout, "Email sent!", Snackbar.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+        }
+        if (v == btnLogout){
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(getApplicationContext(), SignUp_In.class);
             startActivity(intent);
+            finish();
         }
     }
 }
